fad7c8ae9732b94c8048506695e083a39162cef0
Alona Vit <Alona Vit>
2016-06-02 01:03:38 +0300

    Deploy

--- src/main/java/mvc/configuration/AppInitializer.java
+++ src/main/java/mvc/configuration/AppInitializer.java
@@ -1,22 +1,24 @@
 package mvc.configuration;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer implements WebApplicationInitializer {
 
     @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { AppConfig.class };
-    }
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        webApplicationContext.register(AppConfig.class);
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return null;
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] { "/" };
+        ServletRegistration.Dynamic dynamc = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(webApplicationContext));
+        dynamc.addMapping("/*");
+        dynamc.setLoadOnStartup(1);
     }
 
 }
