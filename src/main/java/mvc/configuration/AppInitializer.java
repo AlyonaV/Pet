b9d70bd990c0a506ca1b0292620e3e1db0d509bf
Alona Vit <Alona Vit>
2016-06-12 01:28:39 +0300

    Deploy

--- src/main/java/mvc/configuration/AppInitializer.java
+++ src/main/java/mvc/configuration/AppInitializer.java
@@ -21,7 +21,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] { "/", "" };
+        return new String[] { "/" };
     }
 
     @Override
