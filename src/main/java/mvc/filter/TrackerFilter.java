f6c80f78cb7241b5d8dd178e424e2526689ee7ab
Alona Vit <Alona Vit>
2016-06-12 00:45:11 +0300

    Deploy

--- src/main/java/mvc/filter/TrackerFilter.java
+++ src/main/java/mvc/filter/TrackerFilter.java
@@ -2,6 +2,8 @@ package mvc.filter;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Created by Alona on 11.06.2016.
@@ -16,8 +18,10 @@ public class TrackerFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("FILLLTER");
         System.out.println(servletRequest.getDispatcherType().toString());
-        System.out.println(servletRequest.getAttribute("javax.servlet.forward.request_uri"));
-        System.out.println(servletRequest.getAttribute("javax.servlet.forward.query_string"));
+        Enumeration<String> attributes = servletRequest.getAttributeNames();
+        while(attributes.hasMoreElements()){
+            System.out.println(attributes.nextElement());
+        }
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
