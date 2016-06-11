c5f6fa04e51c34f091e73e00b681e0d1e21d5ffe
Alona Vit <Alona Vit>
2016-06-11 22:41:37 +0300

    Deploy

--- src/main/java/mvc/controller/TrackerController.java
+++ src/main/java/mvc/controller/TrackerController.java
@@ -1,18 +1,12 @@
 package mvc.controller;
 
-import mvc.persistence.dto.Tk103SentenceDTO;
-import mvc.protocol.TK103Protocol;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 @RestController
-@RequestMapping("/")
+@RequestMapping("")
 public class TrackerController {
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
